8ea6322c3c439f5a6e890a53f6c7293e3af16036
jakeasarus <jjones@thinkstack.co>
2020-09-26 07:37:35 -0400

    added bad-config

---
+++ bad-config/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource                                                        "aws_instance "test_instance" {
+  ami                               = "ami-0947d2ba12ee1ff75"
+                     instance_type                            =                               "t2.micro"
+}
+
+resource "aws_instance"         "test_instance_2"     
+                                    ami           = 
+      "ami-0947d2ba12ee1ff75"
+  instance_type     = 
+                                t2.micro"
+      }
+
+# resource          "aws_instance"              "move_to_me" {
+#   ami                 =     "ami-0947d2ba12ee1ff75"
+#   instance_type       =                               "t2.micro"
+#   tags =        {
+#             Name   =            "moved"
+#   }
+# }
