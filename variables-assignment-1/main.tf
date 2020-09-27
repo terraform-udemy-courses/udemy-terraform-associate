0e2e458717e43d515d8462c3f935099d0f097dad
jakeasarus <jjones@thinkstack.co>
2020-09-27 18:41:25 -0400

    made various updates

---
+++ variables-assignment-1/main.tf
@@ -0,0 +1,14 @@
+#Create your provider code block
+#Set a variable for region
+
+
+
+
+#Create an EC2 Instance code block
+#Set a variable for instance_id
+#Set variable for ami_id
+#Set variable for tags HINT: Variable type will be map
+
+
+
+
