5067e7d5fd7a951b3a59865672e6c84620f97fe3
jakeasarus <jjones@thinkstack.co>
2020-09-28 06:59:41 -0400

    added outputs lab 1

---
+++ outputs-lab-1/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "YOUR REGION HERE"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "image_id" {
+    type = string
+    default = "YOUR AMI HERE"
+}
