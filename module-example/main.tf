6b53c15d9dde0ff019e205f56263eba57808051b
jakeasarus <jjones@thinkstack.co>
2020-09-28 21:19:09 -0400

    added modules lab 1

--- module-example/main.tf
+++ module-example/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "YOUR AWS REGION"
 }
 
 module prod_website {
---
+++ modules-lab-1/ec2-module/main.tf
@@ -0,0 +1,9 @@
+#For your module code!!!
+resource "aws_instance" "example" {
+    ami = 
+    instance_type = "t2.micro"
+}
+
+#Put Data for ami filter below
+
+
