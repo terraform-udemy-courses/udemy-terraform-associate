5d9209f08da70878d642690233b6860b4b6a9905
jakeasarus <jjones@thinkstack.co>
2020-10-02 07:26:45 -0400

    added import lab 2

---
+++ import-lab-2/ec2-module/main.tf
@@ -0,0 +1,24 @@
+#For your module code!!!
+resource "aws_instance" "example" {
+    ami = data.aws_ami.windows.id
+    instance_type = "t2.micro"
+}
+
+#AMI Filter for Windows Server 2019 Base
+data "aws_ami" "windows" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["801119661308"] # Canonical
+}
+
+
---
+++ import-lab-2/main.tf
@@ -0,0 +1,7 @@
+#Provider block
+provider "aws" {
+    region = "YOUR REGION HERE"
+}
+
+#EC2 Module Code
+
